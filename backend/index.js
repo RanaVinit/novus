565888a0ebe78a97516b695f561d85f964da0377
RanaVinit <ranavinit1307@gmail.com>
2025-11-20 23:59:54 +0530

    fix: some minor backend fix

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import express from "express";
@@ -8,13 +9,23 @@ const app = express();
 
 dotenv.config();
 
-
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Novus backend :)");
+});
+
+app.get("/blogs", async (req, res) => {
+  try {
+    const data = await fs.promises.readFile(new URL("./db.json", import.meta.url), "utf8");
+    const json = JSON.parse(data);
+    return res.status(200).json(json.blogs ?? json);
+  } catch (err) {
+    console.error("Error reading/parsing db.json:", err);
+    return res.status(500).json({ error: "Failed to read db.json" });
+  }
 });
 
 mongoose.connect(process.env.MONGO_URL)
