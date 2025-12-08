bbfb42e2256bf4c7d0f9dd3827877acb76fe0d26
RanaVinit <ranavinit1307@gmail.com>
2025-12-09 01:22:15 +0530

    feat(model): Add user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,10 +1,22 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Article" }]
+  name: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  }
 });
 
-export default mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
