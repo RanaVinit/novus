e05340b56f7691bfd2b7d9913ef7bf4be122b797
RanaVinit <ranavinit1307@gmail.com>
2026-01-19 02:41:30 +0530

    feat: added vercel origin to CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://novus-gamma.vercel.app"],
   credentials: true,
 }));
 
